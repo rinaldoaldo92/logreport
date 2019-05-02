3b730af1ced989b07c1ca6a4761df218b209ee85
Rinaldo Maharditama <rinaldoaldo92@gmail.com>
2019-05-02 10:15:44 +0700

    first changed
    Perubahan pertama, masih html static dengan dummy data, on progress integrate with database and javascript

---
+++ dashboard/data.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta content='#29C5FF' name='theme-color'>
+	<meta content="#29C5FF" name="msapplication-navbutton-color">
+	<meta content="#29C5FF" name="apple-mobile-web-app-status-bar-style">
+	<meta content="" name="description">
+	<title>Data Site/Client - Dashboard Log Report System Manager</title>
+	<link rel="stylesheet" type="text/css" href="/logreport/assets/style.css">
+	<link rel="stylesheet" type="text/css" href="/logreport/assets/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/logreport/assets/bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="/logreport/assets/js/jquery-3.3.1.js"></script>
+	<script type="text/javascript" src="/logreport/assets/js/popper.min.js"></script>
+	<script type="text/javascript" src="/logreport/assets/bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="/logreport/assets/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<div class="collapse navbar-collapse">
+<ul class="navbar-nav mr-auto">
+	<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+	<li class="nav-item"><a href="task.php" class="nav-link">Task</a></li>
+	<li class="nav-item"><a href="alarm.php" class="nav-link">Alarm</a></li>
+	<li class="nav-item"><a href="query.php" class="nav-link">Query</a></li>
+	<li class="nav-item"><a href="data.php" class="nav-link">Data Site/Client</a></li>
+	<li class="nav-item"><a href="" class="nav-link">Logout</a></li>
+</ul>
+</div>
+</nav>
+<div class="content-dashboard">
+<h2>Data Site/Client Report</h2>
+<div class="table-responsive">
+<table class="table">
+	<thead>
+	<tr>
+		<th>#</th>
+		<th>Code Site/Client</th>
+		<th>Name Site/Client</th>
+		<th>PIC Site/Client</th>
+		<th>Alamat Site/Client</th>
+		<th>Service Class</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td>1</td>
+		<td>ITV009</td>
+		<td>RINAWATI_PASARIBU</td>
+		<td>Rinawati Pasaribu/081366578830</td>
+		<td>Jl Jambu II No 29, Kp. Baris</td>
+		<td>NORMAL_INTERNETTV</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>ATM009</td>
+		<td>BARU_BARIS_APOTEK</td>
+		<td>Joko Maryono/085766480398</td>
+		<td>Jl Mataram Raya No 17, Barisjaya</td>
+		<td>CRITICAL_INTERNET_BANKPERMISI</td>
+	</tr>
+</tbody>
+</table>
+</div>
+</div>
+</body>
+</html>
---
+++ dashboard/task.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta content='#29C5FF' name='theme-color'>
+	<meta content="#29C5FF" name="msapplication-navbutton-color">
+	<meta content="#29C5FF" name="apple-mobile-web-app-status-bar-style">
+	<meta content="" name="description">
+	<title>Task - Dashboard Log Report System Manager</title>
+	<link rel="stylesheet" type="text/css" href="/logreport/assets/style.css">
+	<link rel="stylesheet" type="text/css" href="/logreport/assets/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/logreport/assets/bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="/logreport/assets/js/jquery-3.3.1.js"></script>
+	<script type="text/javascript" src="/logreport/assets/js/popper.min.js"></script>
+	<script type="text/javascript" src="/logreport/assets/bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="/logreport/assets/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<div class="collapse navbar-collapse">
+<ul class="navbar-nav mr-auto">
+	<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+	<li class="nav-item"><a href="task.php" class="nav-link">Task</a></li>
+	<li class="nav-item"><a href="alarm.php" class="nav-link">Alarm</a></li>
+	<li class="nav-item"><a href="query.php" class="nav-link">Query</a></li>
+	<li class="nav-item"><a href="data.php" class="nav-link">Data Site/Client</a></li>
+	<li class="nav-item"><a href="" class="nav-link">Logout</a></li>
+</ul>
+</div>
+</nav>
+<div class="content-dashboard">
+<h2>Task Open</h2>
+<div class="table-responsive">
+<table class="table">
+	<thead>
+	<tr>
+		<th>#</th>
+		<th>ID TT</th>
+		<th>Alarm Detected</th>
+		<th>Created Date</th>
+		<th>Code Site/Client</th>
+		<th>Name Site/Client</th>
+		<th>Status Action</th>
+		<th>Log Action</th>
+		<th>Status TT</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td>1</td>
+		<td>TT-0001-001</td>
+		<td>2019/05/01 15:12</td>
+		<td>2019/05/01 15:30</td>
+		<td>CKD009</td>
+		<td>RINAWATI_PASARIBU</td>
+		<td>SLA Running</td>
+		<td>Up1 01/05/2019 15:32 still coordination with team</td>
+		<td>Open</td>
+	</tr>
+</tbody>
+</table>
+</div>
+</div>
+</body>
+</html>
